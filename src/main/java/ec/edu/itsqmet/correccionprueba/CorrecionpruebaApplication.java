2e8d9d7246d2eecf609d9cb5550371c1647e5850
Darwin Fraga <darwinsvladi@hotmail.com>
2022-12-14 20:49:13 -0500

    creacion proyecto

---
+++ src/main/java/ec/edu/itsqmet/correccionprueba/CorrecionpruebaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.correccionprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecionpruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrecionpruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/itsqmet/correccionprueba/CorrecionpruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.itsqmet.correccionprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrecionpruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
