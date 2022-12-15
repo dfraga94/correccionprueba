3eb460a85fff169f80f7d65b719c1ef0aebbe5b1
Darwin Fraga <darwinsvladi@hotmail.com>
2022-12-14 21:21:24 -0500

    creacion repository

--- src/main/java/ec/edu/itsqmet/correccionprueba/dto/BirdCategoryDTO.java
+++ src/main/java/ec/edu/itsqmet/correccionprueba/dto/BirdCategoryDTO.java
@@ -3,6 +3,7 @@ package ec.edu.itsqmet.correccionprueba.dto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+
 @Entity	(name = "birdcategory")
 public class BirdCategoryDTO {
 
--- src/test/java/ec/edu/itsqmet/correccionprueba/CorrecionpruebaApplicationTests.java
+++ src/test/java/ec/edu/itsqmet/correccionprueba/CorrecionpruebaApplicationTests.java
@@ -1,13 +1,20 @@
 package ec.edu.itsqmet.correccionprueba;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import ec.edu.itsqmet.correccionprueba.repository.IbirdCategoryRepository;
 
 @SpringBootTest
 class CorrecionpruebaApplicationTests {
 
+	 @Autowired
+	private IbirdCategoryRepository birdCategoryRepository;
+	
 	@Test
 	void contextLoads() {
+		System.out.println(birdCategoryRepository.count());
 	}
 
 }
